beaebd2e8f991973d6f171248e5c8cc5efce70a1
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-03-21 18:33:51 -0400

    update: tag

--- builder.go
+++ builder.go
@@ -3,17 +3,17 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"github.com/codefly-dev/core/agents/communicate"
-	basev0 "github.com/codefly-dev/core/generated/go/base/v0"
-	agentv0 "github.com/codefly-dev/core/generated/go/services/agent/v0"
-	"github.com/codefly-dev/core/wool"
-	"path"
-
 	"github.com/codefly-dev/core/agents/services"
 	"github.com/codefly-dev/core/configurations"
+	basev0 "github.com/codefly-dev/core/generated/go/base/v0"
+	agentv0 "github.com/codefly-dev/core/generated/go/services/agent/v0"
 	builderv0 "github.com/codefly-dev/core/generated/go/services/builder/v0"
 	"github.com/codefly-dev/core/shared"
 	"github.com/codefly-dev/core/templates"
+	"github.com/codefly-dev/core/wool"
+	"path"
 )
 
 type Builder struct {
@@ -65,25 +65,33 @@ func (s *Builder) Init(ctx context.Context, req *builderv0.InitRequest) (*builde
 
 	// Get the write mapping
 	writeMapping, err := configurations.FindNetworkMapping(s.write, req.ProposedNetworkMappings)
-
 	if err != nil {
 		return s.Builder.InitError(err)
 	}
 
-	if s.Settings.ReadReplica {
-		s.NetworkMappings = []*basev0.NetworkMapping{writeMapping}
-		// Create a network mapping for read to go to write
-		replica := &basev0.NetworkMapping{
-			Endpoint: s.read,
-			Host:     writeMapping.Host,
-			Port:     writeMapping.Port,
-			Address:  writeMapping.Address,
-		}
-		s.NetworkMappings = append(s.NetworkMappings, replica)
+	writeMapping = configurations.PrefixNetworkMapping(writeMapping, "write")
+
+	readMapping, err := configurations.FindNetworkMapping(s.read, req.ProposedNetworkMappings)
+	if err != nil {
+		return s.Builder.InitError(err)
 	}
+
+	readMapping = configurations.PrefixNetworkMapping(readMapping, "read")
+
+	s.NetworkMappings = []*basev0.NetworkMapping{writeMapping, readMapping}
 
 	s.DependencyEndpoints = req.DependenciesEndpoints
 
+	// This is the credential exposed to dependencies
+	s.ServiceProviderInfos = []*basev0.ProviderInformation{
+		{Name: "redis",
+			Origin: s.Service.Configuration.Unique(),
+			Data: map[string]string{
+				"read":  s.CreateConnectionString(ctx, readMapping.Address),
+				"write": s.CreateConnectionString(ctx, writeMapping.Address),
+			},
+		},
+	}
 	return s.Builder.InitResponse()
 }
 
@@ -100,26 +108,58 @@ func (s *Builder) Sync(ctx context.Context, req *builderv0.SyncRequest) (*builde
 	return s.Builder.SyncResponse()
 }
 
-type Env struct {
-	Key   string
-	Value string
-}
-
-type DockerTemplating struct {
-	Envs []Env
-}
-
 func (s *Builder) Build(ctx context.Context, req *builderv0.BuildRequest) (*builderv0.BuildResponse, error) {
 
 	return &builderv0.BuildResponse{}, nil
 }
 
+type Parameters struct {
+	ReadSelector string
+	SecretMap    services.EnvironmentMap
+}
+
 func (s *Builder) Deploy(ctx context.Context, req *builderv0.DeploymentRequest) (*builderv0.DeploymentResponse, error) {
 	defer s.Wool.Catch()
+
+	// Only expose the "connections"
+
+	info := &basev0.ProviderInformation{
+		Name:   "redis",
+		Origin: s.Configuration.Unique(),
+	}
+	var envs []string
+
+	writeKey := configurations.ProviderInformationEnvKey(info, "write")
+	writeMapping, err := configurations.FindNetworkMapping(s.write, s.NetworkMappings)
+	if err != nil {
+		return s.Builder.DeployError(err)
+	}
+
+	envs = append(envs, fmt.Sprintf("%s=%s", writeKey, s.CreateConnectionString(ctx, writeMapping.Address)))
+
+	readKey := configurations.ProviderInformationEnvKey(info, "read")
+	readMapping, err := configurations.FindNetworkMapping(s.read, s.NetworkMappings)
+	if err != nil {
+		return s.Builder.DeployError(err)
+	}
+
+	envs = append(envs, fmt.Sprintf("%s=%s", readKey, s.CreateConnectionString(ctx, readMapping.Address)))
+
+	secret, err := services.EnvsAsSecretData(envs...)
+	if err != nil {
+		return s.Builder.DeployError(err)
+	}
+
 	base := s.Builder.CreateDeploymentBase(req.Environment, req.Deployment.Namespace, req.BuildContext)
-	params := services.DeploymentTemplateInput{
-		Image:       image,
-		Information: s.Information,
+
+	readSelector := "read"
+	if !s.Settings.ReadReplica {
+		readSelector = "write"
+	}
+
+	params := Parameters{
+		SecretMap:    secret,
+		ReadSelector: readSelector,
 	}
 	switch v := req.Deployment.Kind.(type) {
 	case *builderv0.Deployment_Kustomize:
@@ -139,6 +179,13 @@ func (s *Builder) deployKustomize(ctx context.Context, v *builderv0.Deployment_K
 		services.WithDeployment(deploymentFS, "kustomize/overlays/environment/write").WithDestination(path.Join(destination, "overlays", base.Environment.Name)))
 	if err != nil {
 		return err
+	}
+
+	if s.Settings.ReadReplica {
+		err := s.Templates(ctx, wrapper, services.WithDeployment(deploymentFS, "kustomize/overlays/environment/replicas").WithDestination(path.Join(destination, "overlays", base.Environment.Name)))
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
