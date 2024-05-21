d7fd6d298c6cb49ec6eb6892eb531119c1f724be
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2024-05-21 11:32:52 -0400

    update: tag

--- scripts/build/local.sh
+++ scripts/build/local.sh
@@ -7,3 +7,4 @@ VERSION=$(yq -r '.version' $FILE)
 go mod tidy
 echo Building ${AGENT}:${VERSION}
 go build -o ~/.codefly/agents/services/codefly.dev/${AGENT}__${VERSION} *.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o ~/.codefly/containers/agents/services/codefly.dev/${AGENT}__${VERSION} *.go
